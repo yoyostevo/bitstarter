fd6a585333a2a9edf55294ed25039683abe3a3db
yoyostevo <stevenkennedy2263@gmail.com>
2013-07-12 22:57:45 +0000

    fixed web.js again

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var app = express.createServer(express.logger());
 var buf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8')); };
+  response.send(buf.toString('utf-8'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
