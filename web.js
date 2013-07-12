7c19b2b26a0e438970e79bfd64ffaa763c943171
yoyostevo <stevenkennedy2263@gmail.com>
2013-07-12 22:36:01 +0000

    modified web.js

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(fs.readFileSync("index.html"))
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(buf.toString('utf-8')) }
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
